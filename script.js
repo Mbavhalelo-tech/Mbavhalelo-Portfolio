f08025fe48798f9922c4b0a1b4227f2249fbe1d8
Lufuno Nemudzivhadi <lufuno.nemudzivhadi26@outlook.com>
2025-06-12 09:29:24 +0200

    Add dark mode toggle and welcome alert to portfolio

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
     alert("Welcome to Mbavhalelo's Portfolio!");
 });
+
+const toggleTheme = document.getElementById("toggle-theme");
+
+toggleTheme.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+});
